e0557e4d7259dc0827969eb4eab34ca8d8f545c3
Nelson Taveras <ntaveras03@gmail.com>
2014-05-27 11:12:32 -0400

    Added class variables and initialize method

--- spec/team.rb
+++ spec/team.rb
@@ -2,6 +2,13 @@ require_relative 'player'
 
 class Team
 
+	attr_accessor :teamName, :playersPool
+
+	def initialize()
+		@teamName = 'Default team name'
+		@playersPool = []
+	end
+
 	def set_name(name)
 	end
 
