97d669ceef03dcd297c7af983a7f30d955d4a79d
Nelson Taveras <ntaveras03@gmail.com>
2014-05-27 11:03:04 -0400

    Added base methods for Team class

--- spec/team.rb
+++ spec/team.rb
@@ -1,9 +1,17 @@
+require_relative 'player'
+
 class Team
+
+	def set_name(name)
+	end
 
 	def add_player(name, position)
 	end
 
 	def remove_player(name, position)
+	end
+
+	def show_players()
 	end
 
 	def filter_by_position(position)
