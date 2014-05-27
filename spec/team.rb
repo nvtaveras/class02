d273d8e51519fe35d17bd75c0bf2297972b45c05
Nelson Taveras <ntaveras03@gmail.com>
2014-05-27 11:52:10 -0400

    Refactored some methods from Team Class

--- spec/team.rb
+++ spec/team.rb
@@ -4,24 +4,24 @@ class Team
 
 	attr_accessor :team_name, :players_pool
 
-	def initialize(team_name = 'Default Team Name')
+	def initialize team_name = 'Default Team Name'
 		@team_name = team_name
 		@players_pool = []
 	end
 
-	def add_player(name, position)
+	def add_player name, position
 		@players_pool.push(Player.new(name, position))
 	end
 
-	def remove_player(name, position)
+	def remove_player name, position
 		@players_pool.delete_if { |p| p.name == name && p.position == position }
 	end
 
-	def show_players()
+	def show_players
 		@players_pool.each { |p| puts p.name + ' ' + p.position }
 	end
 
-	def filter_by_position(position)
+	def filter_by_position position
 		@players_pool.each do |p|
 			puts p.name if p.position == position
 		end
