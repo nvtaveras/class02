19d07e7bcf8cbbc5a94904172c9714ea371cc01a
Nelson Taveras <ntaveras03@gmail.com>
2014-05-28 22:56:47 -0400

    Added add_player test with correct params

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,2 +1,18 @@
 require 'spec_helper'
 require 'team'
+
+describe Team do
+
+	describe "#add_player" do
+
+		context "with a new player" do
+			it "should add the player to the current team" do
+				t = Team.new
+				t.add_player('Shaquille Oneal', 'Center')
+				expect t.players_pool.any? { |p| p.name == 'Shaquille Oneal' && p.position == 'Center' }.should == true
+			end
+		end
+
+	end
+
+end
